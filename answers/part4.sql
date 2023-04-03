0a84c5b4aaaa34aff573b8485f8023549302619e
Nguyet <nguyet@zipcodersmbp103.mynetworksettings.com>
2023-04-02 22:58:44 -0400

    added part 4 updated

--- answers/part4.sql
+++ answers/part4.sql
@@ -1,6 +1,15 @@
-SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel, types.name AS primary_type, types.name AS secondary_type
+--SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel, types.name AS primary_type, types.name AS secondary_type
+--FROM pokemons
+--INNER JOIN types ON primary_type = types.id
+--INNER JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
+--INNER JOIN trainers ON trainers.trainerID = pokemon_trainer.trainerID
+--ORDER BY pokemon_trainer.pokelevel DESC;
+
+SELECT pokemons.name, trainers.trainername, pokemon_trainer.pokelevel,
+a.name AS primary_type, b.name AS secondary_type
 FROM pokemons
-INNER JOIN types ON primary_type = types.id
+INNER JOIN types a ON primary_type = a.id
+INNER JOIN types b ON secondary_type = b.id
 INNER JOIN pokemon_trainer ON pokemon_trainer.pokemon_id = pokemons.id
 INNER JOIN trainers ON trainers.trainerID = pokemon_trainer.trainerID
-ORDER BY pokemon_trainer.pokelevel DESC;
+ORDER BY pokemon_trainer.pokelevel DESC limit 10;
